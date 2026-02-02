1983baf64b6ff1dca8af0af2f1b83f5816aac05b
SpeedAmigo <Kxipi7@gmail.com>
2026-02-02 17:13:56 +0100

    jar fragmentations

--- Source/UnrealTopDown/Private/Objects/BasePot.cpp
+++ Source/UnrealTopDown/Private/Objects/BasePot.cpp
@@ -4,10 +4,22 @@
 #include "Objects/BasePot.h"
 
 #include "Characters/Enemies/EnemyDrop.h"
+#include "Characters/Player/PlayerTwinStickCharacter.h"
+#include "GeometryCollection/GeometryCollectionComponent.h"
 
 ABasePot::ABasePot()
 {
 	DropComponent = CreateDefaultSubobject<UEnemyDrop>(TEXT("DropComp"));
+	GeometryCollection->SetEnableDamageFromCollision(false);
+}
+
+void ABasePot::NotifyHit(class UPrimitiveComponent* MyComp, AActor* Other, class UPrimitiveComponent* OtherComp,
+	bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (Other->IsA(APlayerTwinStickCharacter::StaticClass()))
+	{
+		GeometryCollection->SetEnableDamageFromCollision(true);
+	}
 }
 
 void ABasePot::HandleFragmentation()
